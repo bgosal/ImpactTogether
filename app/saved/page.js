3cad23e51974d2884fa408368fd4f47df5f854a8
Jasraj Gosal <jasraj.gosal@student.ufv.ca>
2024-10-29 21:04:21 -0700

    authentication back end

--- app/saved/page.js
+++ app/saved/page.js
@@ -1,34 +1,34 @@
 export default function Saved() {
   return (
     <main>
-      <section class="item-container">
-        <div class="filter-section">
-          <input type="text" placeholder="Search..." class="filter-input"/>
-          <select class="filter-dropdown">
+      <section className="item-container">
+        <div className="filter-section">
+          <input type="text" placeholder="Search..." className="filter-input"/>
+          <select className="filter-dropdown">
             <option value="">Select City</option>
             <option value="langley">Langley</option>
             <option value="vancouver">Vancouver</option>
             <option value="abbotsford">Abbotsford</option>
             <option value="surrey">Surrey</option>
           </select>
-          <select class="filter-dropdown">
+          <select className="filter-dropdown">
             <option value="">Select Category</option>
             <option value="health">Health</option>
             <option value="food">Food</option>
             <option value="community">Community</option>
           </select>
-          <button class="filter-button">Apply Filters</button>
+          <button className="filter-button">Apply Filters</button>
         </div>
 
-        <div class="item-list">
-          <div class="item">
-            {/* <img src="https://via.placeholder.com/100" alt="Item Image" class="item-image"> */}
-            <div class="item-description">
+        <div className="item-list">
+          <div className="item">
+            {/* <img src="https://via.placeholder.com/100" alt="Item Image" className="item-image"> */}
+            <div className="item-description">
               <h3>Item 1</h3>
               <p>This is a short description of item 1. It is designed to give a brief overview.</p>
             </div>
-            <div class="item-buttons">
-              <button class="item-button">Cancel</button>
+            <div className="item-buttons">
+              <button className="item-button">Cancel</button>
             </div>
           </div>
         </div>
